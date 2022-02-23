e47a0de2a712c13c48f6751fddb4afb27bd64ac4
Cristóvão D. Sousa <crisjss@gmail.com>
2022-02-23 22:49:01 +0000

    Output an image

---
+++ devel/main.jl
@@ -0,0 +1,38 @@
+using Images
+using CUDA, KernelAbstractions, CUDAKernels
+using Tullio
+
+function main()
+
+    # Image
+
+    image_width = 256
+    image_height = 256
+
+    # ArrType = Array
+    ArrType = CuArray
+
+    image = ArrType{RGB{Float32}}(undef, image_height, image_width)
+
+    # Render
+
+    function render(i, j)
+        r = (j-1) / (image_width-1)
+        g = (image_height-i) / (image_height-1)
+        b = 0.25
+        RGB(r,g,b)
+    end
+
+    @time begin
+        @tullio image[i,j] = render(i,j)
+        image = ArrType!=Array ? Array(image) : image
+    end
+
+    # Save
+
+    save("image.png", image)
+
+    image
+end
+
+main()
